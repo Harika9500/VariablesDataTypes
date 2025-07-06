9463f1dcecadcb18147d0887077823065a73ebc4
Harika9500 <146800379+Harika9500@users.noreply.github.com>
2025-07-07 04:54:52 +0530

    Update second.js

--- second.js
+++ second.js
@@ -166,17 +166,17 @@ console.log("a+b", c);*/
 // }
 
 
-let marks = 93;
-let grade;
-if(marks>=90 && marks<=100){
-   grade = "A";
-}else if (marks>=70 && marks<=89){
-   grade = "B";
-}else if (marks>=60 && marks<=69){
-    grade = "C";
-}else if (marks>=50 && marks<=59){
-    grade = "D";
-}else if (marks>=0 && marks<=49){
-   grade = "F"; 
-}
-console.log("According to ur scores, your grade was:",grade);
+// let marks = 93;
+// let grade;
+// if(marks>=90 && marks<=100){
+//    grade = "A";
+// }else if (marks>=70 && marks<=89){
+//    grade = "B";
+// }else if (marks>=60 && marks<=69){
+//     grade = "C";
+// }else if (marks>=50 && marks<=59){
+//     grade = "D";
+// }else if (marks>=0 && marks<=49){
+//    grade = "F"; 
+// }
+// console.log("According to ur scores, your grade was:",grade);
